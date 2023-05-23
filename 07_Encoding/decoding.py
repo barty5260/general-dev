52ac86d8abf0d0e32e01175be881c654956cf8b2
Pryviet <62222839+barty5260@users.noreply.github.com>
2023-05-23 21:32:44 +0200

    avv

--- 07_Encoding/decoding.py
+++ 07_Encoding/decoding.py
@@ -2,7 +2,7 @@ import base64
 
 file_path = r'.txt'
 
-file_encode = base64.b64encode(open(file_path, 'rb').read())
+file_encode = base64.b64decode(open(file_path, 'rb').read())
 
 with open(file_path, 'wb') as output:
     output.write(file_encode)
